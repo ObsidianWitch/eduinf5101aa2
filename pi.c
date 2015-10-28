7498f42ef11731f3d37306908841d2e709ef3ba7
Obside <obsiagae@gmail.com>
2015-10-28 04:07:49 +0100

    clean pi

--- pi.c
+++ pi.c
@@ -4,39 +4,38 @@
 
 #include "mpi.h"
 
-double arctan(double a, double b, double n){
+const double INTERVALS = 1.0e8;
+
+double arctan(double a, double b, double n) {
 	double sum = 0;
-	for(double x=a; x<b; x+=1./n){
-		sum += (1./(1.+x*x)) * 1./n;
+	for (double x = a ; x < b ; x += 1./n) {
+		sum += (1. / (1. + x*x)) * 1./n;
 	}
 	return sum;
 }
 
 int main(int argc, char** argv) {
 	int size, rank;
-	double localResult;
-	double reduceBuffer;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
-	double drank = (double)rank;
-	double dsize = (double)size;
-	double a = drank/dsize;
-	double b = (drank+1)/dsize;
 	
-	localResult = 4*arctan(a,b,10000000000000.);
+	double a = (double) rank / size;
+	double b = (double) (rank + 1) / size;
+	double localResult = 4 * arctan(a, b, INTERVALS);
 	
-/*	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);*/
-	MPI_Reduce(&localResult, &reduceBuffer, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-/*	printf("{%d} PI[%.2f,%.2f] = %f\n",rank, a, b, 4*arctan(a,b,10));*/
-/*	printf("%f\n",);*/
+	double reduceBuffer;
+	MPI_Reduce(
+		&localResult, &reduceBuffer, 1,
+		MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD
+	);
 	
-	
-	MPI_Finalize();
-	if(rank == 0){
+	if (rank == 0) {
 		printf("%.42f\n", reduceBuffer);
 		printf("%.42f\n", M_PI);
 	}
+	
+	MPI_Finalize();
 	
 	return EXIT_SUCCESS;
 }
