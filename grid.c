a97cc67c876a810867dd6e8464f35864849ba1d2
Obside <obsiagae@gmail.com>
2015-10-28 14:20:22 +0100

    grid argv

--- grid.c
+++ grid.c
@@ -3,15 +3,23 @@
 #include "mpi.h"
 #include "tools/local_matrix.h"
 
-const int N_MATRIX = 12;
-
 int main(int argc, char** argv) {
     int nprocs, rank;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    
+    if (argc < 2) {
+        printf(
+            "Missing arguments\n"
+            "Usage : %s N_MATRIX\n",
+            argv[0]
+        );
+        return EXIT_FAILURE;
+    }
+    int nmatrix = atoi(argv[1]);
 
-    LocalMatrix localMatrix = createLocalMatrix(nprocs, N_MATRIX, rank);
+    LocalMatrix localMatrix = createLocalMatrix(nprocs, nmatrix, rank);
 
     MPI_Barrier(MPI_COMM_WORLD);
 
